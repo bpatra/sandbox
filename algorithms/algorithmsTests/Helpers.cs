e8716818779ba5ce26647b776205b12336481ad9
Benoît Patra <benoit.patra@gmail.com>
2012-11-24 08:14:42 +0100

    generic extension method for testing if array or items are sorted

--- algorithms/algorithmsTests/Helpers.cs
+++ algorithms/algorithmsTests/Helpers.cs
@@ -16,6 +16,18 @@ namespace algorithmsTests
             return true;
         }
 
+        public static bool IsSorted<T>(this T[] array) where T : IComparable<T>
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1].CompareTo(array[i])  > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         public static void Print(this int[] array)
         {
             for (int i = 0; i < array.Length; i++)
