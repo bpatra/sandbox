581ad8110a727fb64f409df2a3a0187d464659de
Benoît Patra <benoit.patra@gmail.com>
2012-11-24 15:38:40 +0100

    implement bubble sort

---
+++ algorithms/algorithms/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace algorithms
+{
+    public class BubbleSort<T> : ISortable<T> where T : IComparable<T>
+    {
+        public void Sort(T[] items)
+        {
+            for(int i=0; i < items.Length; i++)
+            {
+                for(int j= items.Length -1; j >= i+1; j--)
+                {
+                    if(items[j-1].CompareTo(items[j]) >= 1)
+                    {
+                        var val = items[j];
+                        items[j] = items[j - 1];
+                        items[j - 1] = val;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ algorithms/algorithmsTests/BubbleSortTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algorithmsTests
+{
+    /// <summary>
+    /// Summary description for BubbleSortTests
+    /// </summary>
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestMethod]
+        public void BasicSort()
+        {
+            var gen = new Random(44);
+            var iSort = new BubbleSort<Mock>();
+            foreach (var N in new[] { 1, 3, 5, 10 })
+            {
+                var mocks = Enumerable.Range(0, N).Select(i => new Mock(gen.Next(0, 10))).ToArray();
+                iSort.Sort(mocks);
+                Assert.IsTrue(mocks.IsSorted(), "#C" + N);
+            }
+        }
+    }
+}
