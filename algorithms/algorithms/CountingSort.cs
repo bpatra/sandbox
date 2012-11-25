95ea3c432eeea1ef41314f29daa99cdbc550f4b9
Benoît Patra <benoit.patra@gmail.com>
2012-11-25 17:39:00 +0100

    add counting sort algorithm

---
+++ algorithms/algorithms/CountingSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace algorithms
+{
+    public class CountingSort : ISortable<int>
+    {
+        private int _k;
+        /// <param name="k">max exclusive expected values for items.</param>
+        public CountingSort(int k)
+        {
+            _k = k;
+        }
+        
+        public void Sort(int[] items)
+        {
+            var count = new int[_k];
+            var tempItems = new int[items.Length];
+
+            for (int i = 0; i < items.Length; i++ )
+            {
+                Debug.Assert(items[i] <= _k, "all value are expected to be lower or equal "+_k);
+                count[items[i]]++;
+            }
+            
+            for(int i=1; i < _k; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for(int j =items.Length-1; j>=0; j--)
+            {
+                var target = count[items[j]];
+                tempItems[--target] = items[j];
+                count[items[j]]--;
+            }
+
+            Array.Copy(tempItems,items, items.Length);
+        }
+    }
+}
---
+++ algorithms/algorithmsTests/CountingSortTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algorithmsTests
+{
+    /// <summary>
+    /// Summary description for CountingSortTests
+    /// </summary>
+    [TestClass]
+    public class CountingSortTests
+    {
+        [TestMethod]
+        public void BasicSort()
+        {
+            var items = new[] {1};
+            var countingSort = new CountingSort(2);
+            //countingSort.Sort(new int[]{});
+            //countingSort.Sort(items);
+            //Assert.AreEqual(1, items[0], "#Z01");
+
+            countingSort = new CountingSort(7);
+            items = new[] {5, 6, 6, 3, 2, 1, 3, 4};
+            countingSort.Sort(items);
+            Assert.IsTrue(items.IsSorted(), "#Z02");
+        }
+    }
+}
