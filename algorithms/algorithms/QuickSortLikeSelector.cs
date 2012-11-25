dbf276043914a4f341beda840a2ecd8e5d38e9a7
Benoît Patra <benoit.patra@gmail.com>
2012-11-25 19:06:36 +0100

    empirical quantile selector with "quicksort"-like underneath implementation

---
+++ algorithms/algorithms/QuickSortLikeSelector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace algorithms
+{
+    public class QuickSortLikeSelector<T>  where T : IComparable<T>
+    {
+        private T[] _items;
+        private const int Seed = 199;
+        private Random _gen;
+        private int _i;
+
+        public T Select(T[] items, int i)
+        {
+            _items = (T[]) items.Clone();
+            _gen = new Random(Seed);
+            _i = i - 1;
+
+            return Select(0, items.Length - 1);
+        }
+
+        private T Select(int start, int end)
+        {
+            int k = Partition(start, end); 
+
+            if(k < _i)
+            {
+                return Select(k +1, end);
+            }
+            else if(k == _i)
+            {
+                return _items[k];
+            }
+            else
+            {
+                return Select(start, k -1);
+            }
+        }
+
+        private int Partition(int start, int end)
+        {
+            int pivotIndex = _gen.Next(start, end + 1);
+            Swap(pivotIndex, end);
+
+            int j = start;
+            for(int i=start; i <= end -1; i++)
+            {
+                if(_items[i].CompareTo(_items[j]) < 1)
+                {
+                    Swap(i, j);
+                    j++;
+                }
+            }
+            Swap(j, end);
+            return j;
+        }
+
+        private void Swap(int i, int j)
+        {
+            var val = _items[i];
+            _items[i] = _items[j];
+            _items[j] = val;
+        }
+    }
+}
---
+++ algorithms/algorithmsTests/QuickSortLikeSelectorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algorithmsTests
+{
+    /// <summary>
+    /// Summary description for QuickSortLikeSelectorTests
+    /// </summary>
+    [TestClass]
+    public class QuickSortLikeSelectorTests
+    {
+    
+        [TestMethod]
+        public void Select()
+        {
+            var selector = new QuickSortLikeSelector<MockComparable>();
+            var result = selector.Select(new[] {new MockComparable(3)} , 1);
+            Assert.AreEqual(3, result.Value, "#H1");
+
+            var selector2 = new QuickSortLikeSelector<int>();
+            var array = new int[] {4, 5, 1, 2, 2, 8, 10, 3, 1};
+            var result2 = selector2.Select( array , 3);
+            Assert.AreEqual(2, result2,"#H2");
+            var max = selector2.Select(array, array.Length);
+            Assert.AreEqual(10, max, "#H3");
+        }
+    }
+}
