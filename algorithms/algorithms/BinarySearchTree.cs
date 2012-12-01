e33c70c28da194a1f1f0a90e4761308619c5758c
Benoît Patra <benoit.patra@gmail.com>
2012-12-01 16:42:35 +0100

    implement CollectInOrder, Maximum, Minimum and Insert for BinarySearchTree

--- algorithms/algorithms/BinarySearchTree.cs
+++ algorithms/algorithms/BinarySearchTree.cs
@@ -7,50 +7,112 @@ namespace algorithms
 {
     public class BinarySearchTree<T> where T: IComparable<T>
     {
-        //private TNode<T> _root;
+        private TNode<T> _root;
         
-        IEnumerable<T> CollectInOrder(TNode<T> root)
+        public List<T> CollectInOrder(TNode<T> startRoot)
+        {
+            var list = new List<T>();
+            CollectInOrder(list, startRoot);
+            return list;
+        }
+
+        public List<T> CollectInOrder()
+        {
+            return CollectInOrder(_root);
+        }
+
+        private void CollectInOrder(List<T> list, TNode<T> startRoot)
+        {
+            if(startRoot != null)
+            {
+                CollectInOrder(list, startRoot.LeftChild);
+                list.Add(startRoot.Content);
+                CollectInOrder(list, startRoot.RightChild);
+            }
+        }
+
+        public TNode<T> Search(T item)
         {
             throw new NotImplementedException();
         }
 
-        TNode<T> Search(T item)
+        public TNode<T> Minimum()
+        {
+            return Minimum(_root);
+        }
+
+
+        public TNode<T> Minimum(TNode<T> startNode)
+        {
+            if(startNode.LeftChild == null)
+            {
+                return startNode;
+            }
+            else
+            {
+                return Minimum(startNode.LeftChild);
+            }
+        }
+
+        public TNode<T> Maximum()
+        {
+            return Maximum(_root);
+        }
+
+        public TNode<T> Maximum(TNode<T> node)
+        {
+           if(node.RightChild == null)
+           {
+               return node;
+           }
+           else
+           {
+               return Maximum(node.RightChild);
+           }
+        }
+
+        public TNode<T> Sucessor(TNode<T> node)
         {
             throw new NotImplementedException();
         }
 
-        TNode<T> Minimum()
+        public void Insert(TNode<T> node)
         {
-            throw new NotImplementedException();
+
+            var currentNode = _root;
+            TNode<T> parentCurrentNode = null;
+            while(currentNode != null)
+            {
+                parentCurrentNode = currentNode;
+                if(currentNode.Content.CompareTo(node.Content) < 1)
+                {
+                    currentNode = currentNode.RightChild;
+                }
+                else
+                {
+                    currentNode = currentNode.LeftChild;
+                }
+            }
+
+            if(parentCurrentNode==null)
+            {
+                _root = node;
+            }
+            else
+            {
+                node.Parent = parentCurrentNode;
+                if(parentCurrentNode.Content.CompareTo(node.Content) < 1)
+                {
+                    parentCurrentNode.RightChild = node;
+                }
+                else
+                {
+                    parentCurrentNode.LeftChild = node;
+                }
+            }
         }
 
-
-        TNode<T> Minimum(TNode<T> root)
-        {
-            throw new NotImplementedException();
-        }
-
-        TNode<T> Maximum()
-        {
-            throw new NotImplementedException();
-        }
-
-        TNode<T> Maximum(TNode<T> root)
-        {
-            throw new NotImplementedException();
-        }
-
-        TNode<T> Sucessor(TNode<T> node)
-        {
-            throw new NotImplementedException();
-        }
-
-        void Insert(TNode<T> node)
-        {
-            throw  new NotImplementedException();
-        }
-
-        void Delete(TNode<T> node)
+        public void Delete(TNode<T> node)
         {
             throw  new NotImplementedException();
         }
---
+++ algorithms/algorithmsTests/BinarySearchTreeTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace algorithmsTests
+{
+    /// <summary>
+    /// Summary description for BinarySearchTreeTests
+    /// </summary>
+    [TestClass]
+    public class BinarySearchTreeTests
+    {
+
+        [TestMethod]
+        public void Insert()
+        {
+            var tree = new BinarySearchTree<MockComparable>();
+            var mock = new MockComparable(3);
+            var node = new TNode<MockComparable> { Content = mock };
+            tree.Insert(node);
+
+            Assert.AreEqual(3, tree.Maximum().Content.Value, "#A01");
+            Assert.AreEqual(3, tree.Minimum().Content.Value, "#A01bis");
+
+            var mock2 = new MockComparable(7);
+            var node2 = new TNode<MockComparable> { Content = mock2 };
+            tree.Insert(node2);
+            Assert.AreEqual(7, tree.Maximum().Content.Value, "#A02");
+            Assert.AreEqual(3, tree.Minimum().Content.Value, "#A02bis");
+        }
+
+        [TestMethod]
+        public void InOrder()
+        {
+            var tree = new BinarySearchTree<int>();
+            tree.Insert(new TNode<int>{Content = 8});
+            tree.Insert(new TNode<int>{ Content = 15 });
+            tree.Insert(new TNode<int> { Content = 5});
+            tree.Insert(new TNode<int> { Content = 17 });
+            tree.Insert(new TNode<int> { Content = 14 });
+
+            var content = new int[] {5, 8, 14, 15, 17};
+            var result = tree.CollectInOrder().ToArray();
+            Assert.AreEqual(content.Length, result.Length, "#B01");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(content[i], result[i], "#B"+i);
+            }
+
+        }
+    }
+}
