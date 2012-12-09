99ca40ce02f04285fb8efd2122a146be7a92a549
unknown <bpatr@EFRONT-0-00199.frontlabs.com>
2012-12-09 17:01:06 +0100

    add graph class. Implement a slow Add method to add a vertex and its associated edges.

---
+++ algorithms/algorithms/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace algorithms
+{
+    public class Graph<T>
+    {
+        LList<T>[] _graph;
+        public Graph()
+        {
+            _graph = new LList<T>[0];
+        }
+
+        public void Add(T target, IEnumerable<T> neighbors)
+        {
+            var newGraph = new LList<T>[_graph.Length + 1];
+            Array.Copy(_graph, newGraph, _graph.Length);
+
+            var linkedList = new LList<T>();
+            foreach (var neighbor in neighbors)
+            {
+                linkedList.Add(neighbor);
+            }
+            newGraph[newGraph.Length - 1] = linkedList;
+        }
+
+        public IEnumerable<T> BFS()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
